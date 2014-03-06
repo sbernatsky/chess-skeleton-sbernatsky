8499e52a82cb3b7f8d5579ec17f6d5cced357c07
sbernatsky <sbernatsky@ukr.net>
2014-03-06 18:32:41 +0200

    moves implementation for knight

--- src/main/java/chess/pieces/Knight.java
+++ src/main/java/chess/pieces/Knight.java
@@ -1,6 +1,12 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The Knight class
@@ -13,5 +19,25 @@ public class Knight extends Piece {
     @Override
     protected char getIdentifyingCharacter() {
         return 'n';
+    }
+
+    public Collection<Position> getMoves(GameState state, Position position) {
+        List<Position> tmp = new ArrayList<Position>();
+        tmp.add(new Position((char) (position.getColumn() + 1), position.getRow() + 2));
+        tmp.add(new Position((char) (position.getColumn() + 1), position.getRow() - 2));
+        tmp.add(new Position((char) (position.getColumn() - 1), position.getRow() + 2));
+        tmp.add(new Position((char) (position.getColumn() - 1), position.getRow() - 2));
+        tmp.add(new Position((char) (position.getColumn() + 2), position.getRow() + 1));
+        tmp.add(new Position((char) (position.getColumn() + 2), position.getRow() - 1));
+        tmp.add(new Position((char) (position.getColumn() - 2), position.getRow() + 1));
+        tmp.add(new Position((char) (position.getColumn() - 2), position.getRow() - 1));
+
+        List<Position> result = new ArrayList<Position>();
+        for (Position p : tmp) {
+            if (p.isValid() && state.getOwnerAt(p) != getOwner()) {
+                result.add(p);
+            }
+        }
+        return result;
     }
 }
