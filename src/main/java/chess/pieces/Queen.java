a0d58a3a7ee9075fba8fb0e737ead71ad15a9157
sbernatsky <sbernatsky@ukr.net>
2014-03-06 18:14:33 +0200

    moves implementation for queen

--- src/main/java/chess/pieces/Queen.java
+++ src/main/java/chess/pieces/Queen.java
@@ -1,6 +1,12 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The Queen
@@ -13,5 +19,22 @@ public class Queen extends Piece{
     @Override
     protected char getIdentifyingCharacter() {
         return 'q';
+    }
+
+    @Override
+    public Collection<Position> getMoves(GameState state, Position position) {
+        List<Position> positions = new ArrayList<Position>();
+
+        positions.addAll(Util.collectMovesNorth(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesSouth(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesEast(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesWest(state, getOwner(), position, 8));
+
+        positions.addAll(Util.collectMovesNorthEast(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesNorthWest(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesSouthEast(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesSouthWest(state, getOwner(), position, 8));
+
+        return positions;
     }
 }
