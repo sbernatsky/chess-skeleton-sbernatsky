be68f969f997a5ad3f4964d3c119449e046412a2
sbernatsky <sbernatsky@ukr.net>
2014-03-06 18:15:19 +0200

    moves implementation for bishop

--- src/main/java/chess/pieces/Bishop.java
+++ src/main/java/chess/pieces/Bishop.java
@@ -1,6 +1,12 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The 'Bishop' class
@@ -13,5 +19,17 @@ public class Bishop extends Piece {
     @Override
     protected char getIdentifyingCharacter() {
         return 'b';
+    }
+
+    @Override
+    public Collection<Position> getMoves(GameState state, Position position) {
+        List<Position> positions = new ArrayList<Position>();
+
+        positions.addAll(Util.collectMovesNorthEast(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesNorthWest(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesSouthEast(state, getOwner(), position, 8));
+        positions.addAll(Util.collectMovesSouthWest(state, getOwner(), position, 8));
+
+        return positions;
     }
 }
